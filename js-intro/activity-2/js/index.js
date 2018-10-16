3844a0eacd62c5773e5b99274f449db0e27f6c5a
mkfreeman <michael.k.freeman@gmail.com>
2018-10-16 10:54:18 -0700

    Added more instructions to activity 2

--- js-intro/activity-2/js/index.js
+++ js-intro/activity-2/js/index.js
@@ -17,10 +17,42 @@ ctx.strokeStyle = 2;
 let input = document.getElementById('textInput');
 input.onkeydown = drawLines;
 
+// Function to return an array of lengths from the text
+function getLinesFromText() {
+    // Get the value of the text input
+    let text = document.getElementById('textInput').value;
 
-// Function to draw lines
-function drawLines() {
-
+    // Return an *array* containing the length of each word
 }
 
 
+// Function to draw lines
+function drawLines() {
+    // Clear rectangle and begin path
+    ctx.clearRect(0, 0, width, height);
+    ctx.beginPath();
+
+    // Set initial position (x, y) in the center of the canvas
+    let position = {};
+
+    // Move the pen to the starting position
+
+
+    // Get the array of lengths from text input using `getLinesFromText()`
+
+
+    // Iterate through lengths, updating and tracking the pen position
+    // Turn 90 degrees each turn
+
+    // -- Compute a direction 
+
+    // -- Add different x or y amount based on direction using switch/case
+
+    // -- Update the `position` by adding x and y
+
+
+    // -- Move the pen to the new position
+
+    // Draw the canvas using the `stroke` method
+
+}
