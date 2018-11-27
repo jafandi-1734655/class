512e9442afa1cb4405650e1dae1458840bfb67d7
mkfreeman <michael.k.freeman@gmail.com>
2018-11-27 14:24:30 -0800

    Fix typo

--- firebase-storage/src/App.js
+++ firebase-storage/src/App.js
@@ -10,35 +10,35 @@ class App extends Component {
         }
     }
     componentDidMount() {
-        // Create references to the 'imgs' endpoings on database and storage
-        
+        // Create references to the 'imgs' endpoints on database and storage
+
 
         // When the database 'value' changes, change the state of `imgs`
-        
+
     }
 
     // Event when the input changes
     fileChange(event) {
         // Get the uploaded file and its name
-        
+
 
         // Create a new child reference (on storage) for the image using its name
         // Then, `put` the file contents. *then()*
         // Using the `snapshot.downloadURL` value, push a new element into 
         // The _database_ `imgs` reference
-        
+
 
     }
     render() {
         return (
             <div className="App">
-              <div>
-                <input type="file" onChange={ (e) => this.fileChange(e) } />
-              </div>
-              { /* Iterate through the keys of the `imgs` state and render an image*/ }
-              
+                <div>
+                    <input type="file" onChange={(e) => this.fileChange(e)} />
+                </div>
+                { /* Iterate through the keys of the `imgs` state and render an image*/}
+
             </div>
-            );
+        );
     }
 }
 
